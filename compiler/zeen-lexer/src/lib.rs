6eabfe722d0d045f15b8e73922b7301b647f8f8b
mealet <jasonragonter@gmail.com>
2026-05-08 21:01:04 +0300

    feat: added whitespaces and comments skip fn in lexer

--- compiler/zeen-lexer/src/lib.rs
+++ compiler/zeen-lexer/src/lib.rs
@@ -1,8 +1,10 @@
 #![allow(unused)]
 
 use std::str::Chars;
-pub use token::Token;
 
+pub use token::{Token, TokenKind};
+
+mod tests;
 pub mod token;
 
 pub fn tokenize(input: &str) -> impl Iterator<Item = Token> {
@@ -10,7 +12,7 @@ pub fn tokenize(input: &str) -> impl Iterator<Item = Token> {
 
     std::iter::from_fn(move || {
         let token = tokenizer.advance_token();
-        if token.kind != token::TokenKind::Eof {
+        if token.kind != TokenKind::Eof {
             Some(token)
         } else {
             None
@@ -83,9 +85,53 @@ impl<'inp> Tokenizer<'inp> {
         self.chars = self.as_str()[n..].chars();
     }
 
+    fn skip_whitespace(&mut self) {
+        while (!self.is_eof()) {
+            let chr = self.first();
+
+            if (chr.is_whitespace()) {
+                if self.bump().is_none() {
+                    break;
+                }
+            } else if chr == '/' {
+                match self.second() {
+                    // line comment
+                    '/' => {
+                        while !self.is_eof() {
+                            if self.bump() == Some('\n') {
+                                break;
+                            }
+                        }
+                    }
+
+                    // block comment
+                    '*' => {
+                        while !self.is_eof() && !(self.first() == '*' && self.second() == '/') {
+                            if self.bump() == None {
+                                break;
+                            }
+                        }
+
+                        self.bump_n(2);
+                    }
+
+                    _ => break,
+                }
+            } else {
+                break;
+            }
+        }
+    }
+
     // Tokenizers
 
     pub fn advance_token(&mut self) -> Token {
+        self.skip_whitespace();
+
+        let Some(first_char) = self.bump() else {
+            return Token::new(TokenKind::Eof, (0, 0).into());
+        };
+
         todo!();
     }
 }
