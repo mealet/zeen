a734c715915579640276c1f8f6e28c243ecc0b88
mealet <jasonragonter@gmail.com>
2026-05-08 19:08:26 +0300

    added MietteDriver struct to zeen-driver

--- compiler/zeen-driver/src/lib.rs
+++ compiler/zeen-driver/src/lib.rs
@@ -2,3 +2,26 @@ mod span;
 
 pub use span::LineOffsets;
 pub use span::LocationSpan;
+
+pub struct MietteDriver {
+    reporter: miette::GraphicalReportHandler,
+}
+
+impl MietteDriver {
+    pub fn new() -> Self {
+        let reporter = miette::GraphicalReportHandler::new()
+            .tab_width(2)
+            .with_links(true)
+            .with_cause_chain();
+
+        Self { reporter }
+    }
+
+    pub fn report(&self, diagnostic: &dyn miette::Diagnostic) -> Result<String, std::fmt::Error> {
+        let mut buffer = String::new();
+
+        self.reporter.render_report(&mut buffer, diagnostic)?;
+
+        return Ok(buffer);
+    }
+}
