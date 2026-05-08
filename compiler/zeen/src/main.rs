ea2a2387bcd595c39e50b381bf62f7819331469c
mealet <jasonragonter@gmail.com>
2026-05-08 20:46:41 +0300

    test: added testing setup in main exe

--- compiler/zeen/src/main.rs
+++ compiler/zeen/src/main.rs
@@ -1,3 +1,26 @@
+use zeen_driver::MietteDriver;
+
+use miette::{Diagnostic, NamedSource, SourceSpan};
+use thiserror::Error;
+
+#[derive(Debug, Error, Diagnostic)]
+#[error("test error")]
+struct TestError {
+    #[source_code]
+    src: NamedSource<String>,
+    #[label("here")]
+    span: SourceSpan,
+}
+
 fn main() {
-    println!("Hello, world!");
+    const SRC: &str = "123 hello 123";
+
+    let err = TestError {
+        src: NamedSource::new("test.zn", SRC.into()),
+        span: SourceSpan::new(4.into(), 5),
+    };
+
+    let driver = MietteDriver::new();
+
+    println!("{}", driver.report(&err).unwrap());
 }
