13daabb898ad18089980900a6226210d2dbcce9c
mealet <jasonragonter@gmail.com>
2026-05-12 20:02:20 +0300

    feat: added compiler types and compiler keywords to tokens kinds

--- compiler/zeen-lexer/src/token.rs
+++ compiler/zeen-lexer/src/token.rs
@@ -16,10 +16,9 @@ impl Token {
 pub enum TokenKind {
     Ident,      // abcd
     MacroIdent, // print!
-    Keyword,    // `if`, `defer` and etc...
     Ref,        // &expr
 
-    Type(),
+    Type(CompilerType),
     Literal { kind: LiteralKind },
 
     Underscore, // _
@@ -43,6 +42,8 @@ pub enum TokenKind {
 
     LShift, // <<
     RShift, // >>
+
+    FatArrow, // =>
 
     Pipe,      // |
     Ampersand, // &
@@ -85,4 +86,136 @@ pub enum IntBase {
     Hexadecimal = 16,
 }
 
-pub enum CompilerType {}
+#[allow(non_camel_case_types)]
+#[derive(Debug, PartialEq)]
+pub enum CompilerType {
+    // signed integers
+    i8,
+    i16,
+    i32,
+    i64,
+    isize,
+
+    // unsigned integers
+    u8,
+    u16,
+    u32,
+    u64,
+    usize,
+
+    // float types
+    f32,
+    f64,
+
+    // others
+    bool,
+    char,
+    void,
+}
+
+impl CompilerType {
+    pub fn from_str(str: impl AsRef<str>) -> Option<Self> {
+        match str.as_ref() {
+            "i8" => Some(CompilerType::i8),
+            "i16" => Some(CompilerType::i16),
+            "i32" => Some(CompilerType::i32),
+            "i64" => Some(CompilerType::i64),
+            "isize" => Some(CompilerType::isize),
+
+            "u8" => Some(CompilerType::u8),
+            "u16" => Some(CompilerType::u16),
+            "u32" => Some(CompilerType::u32),
+            "u64" => Some(CompilerType::u64),
+            "usize" => Some(CompilerType::usize),
+
+            "f32" => Some(CompilerType::f32),
+            "f64" => Some(CompilerType::f64),
+
+            "bool" => Some(CompilerType::bool),
+            "char" => Some(CompilerType::bool),
+            "void" => Some(CompilerType::bool),
+
+            _ => None,
+        }
+    }
+}
+
+impl std::fmt::Display for CompilerType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+pub enum CompilerKeyword {
+    If,
+    Else,
+    While,
+    For,
+    Break,
+
+    Let,
+    Const,
+    Defer,
+    Switch,
+    Return,
+
+    Pub,
+    Fn,
+    Extern,
+    Include,
+    Link,
+    Import,
+    Struct,
+    Enum,
+
+    Interface,
+    Implement,
+    Type,
+
+    True,
+    False,
+    Null,
+
+    SelfLower,
+    SelfUpper,
+}
+
+impl CompilerKeyword {
+    pub fn from_str(str: impl AsRef<str>) -> Option<Self> {
+        match str.as_ref() {
+            "if" => Some(CompilerKeyword::If),
+            "else" => Some(CompilerKeyword::Else),
+            "while" => Some(CompilerKeyword::While),
+            "for" => Some(CompilerKeyword::For),
+            "break" => Some(CompilerKeyword::Break),
+
+            "let" => Some(CompilerKeyword::Let),
+            "const" => Some(CompilerKeyword::Const),
+            "defer" => Some(CompilerKeyword::Defer),
+            "switch" => Some(CompilerKeyword::Switch),
+            "return" => Some(CompilerKeyword::Return),
+
+            "pub" => Some(CompilerKeyword::Pub),
+            "fn" => Some(CompilerKeyword::Fn),
+            "extern" => Some(CompilerKeyword::Extern),
+            "include" => Some(CompilerKeyword::Include),
+            "link" => Some(CompilerKeyword::Link),
+            "import" => Some(CompilerKeyword::Import),
+            "struct" => Some(CompilerKeyword::Struct),
+            "enum" => Some(CompilerKeyword::Enum),
+
+            "interface" => Some(CompilerKeyword::Interface),
+            "implement" => Some(CompilerKeyword::Implement),
+            "type" => Some(CompilerKeyword::Type),
+
+            "true" => Some(CompilerKeyword::True),
+            "false" => Some(CompilerKeyword::False),
+            "null" => Some(CompilerKeyword::Null),
+
+            "self" => Some(CompilerKeyword::SelfLower),
+            "Self" => Some(CompilerKeyword::SelfUpper),
+
+            _ => None,
+        }
+    }
+}
