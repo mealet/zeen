bbc05878d53071300368e1511285363d0a56e620
mealet <jasonragonter@gmail.com>
2026-05-13 18:57:18 +0300

    test: implemented test setup in main exe

--- compiler/zeen/src/main.rs
+++ compiler/zeen/src/main.rs
@@ -18,21 +18,37 @@ struct SrcDebugger {
 }
 
 fn main() {
-    const SRC: &str = "implement";
+    let path = std::env::args().nth(1).unwrap_or_else(|| {
+        eprintln!("No path to file found");
 
-    let tokens = zeen_lexer::tokenize(SRC);
+        std::process::exit(1);
+    });
 
-    let driver = MietteDriver::new();
+    match std::fs::read_to_string(&path) {
+        Ok(content) => {
+            let driver = MietteDriver::new();
+            let tokens = zeen_lexer::tokenize(&content);
 
-    for tok in tokens {
-        let diagnostic = driver
-            .report(&SrcDebugger {
-                dbg: format!("{:?}", tok.kind),
-                src: NamedSource::new("debug", SRC.into()),
-                span: tok.span,
-            })
-            .unwrap();
+            let start = std::time::Instant::now();
 
-        println!("{}", diagnostic);
+            for token in tokens {
+                if token.kind == zeen_lexer::TokenKind::Unknown {
+                    let rep = driver
+                        .report(&SrcDebugger {
+                            dbg: format!("Unknown token"),
+                            src: NamedSource::new(&path, content.clone()),
+                            span: token.span,
+                        })
+                        .unwrap();
+
+                    eprintln!("{}", rep);
+                    std::process::exit(1);
+                }
+            }
+        }
+        Err(err) => {
+            eprintln!("Unable to open file: {}", err);
+            std::process::exit(1);
+        }
     }
 }
