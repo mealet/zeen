0c55e895dfad1687018d3254913dec05348e6948
mealet <jasonragonter@gmail.com>
2026-05-13 23:04:07 +0300

    docs: added comment about references in ast

--- compiler/zeen-ast/src/lib.rs
+++ compiler/zeen-ast/src/lib.rs
@@ -4,3 +4,7 @@ pub mod declarations;
 pub mod expressions;
 pub mod statements;
 
+// NOTE: Zeen AST relies on external arena allocator (to avoid separated heap pointers like
+// Box/Rc/Arc/...). So expressions/statements/declarations must keep lifetimed references to other
+// members instead of "boxing" them on heap.
+
