895958c078a75100a4965f8c5757926942e7f454
mealet <jasonragonter@gmail.com>
2026-05-14 11:23:45 +0300

    feat: added public exports to ast module

--- compiler/zeen-ast/src/lib.rs
+++ compiler/zeen-ast/src/lib.rs
@@ -5,6 +5,11 @@ pub mod expressions;
 pub mod statements;
 pub mod types;
 
+pub use declarations::{Declaration, DeclarationKind};
+pub use expressions::{Expression, ExpressionKind};
+pub use statements::{Statement, StatementKind};
+pub use types::{TypeExpr, TypeKind};
+
 // NOTE: Zeen AST relies on external arena allocator (to avoid separated heap pointers like
 // Box/Rc/Arc/...). So expressions/statements/declarations must keep lifetimed references to other
 // members instead of "boxing" them on heap.
