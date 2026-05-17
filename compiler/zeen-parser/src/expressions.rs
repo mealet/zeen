b22d858ac8bb49423e62e0373fd4a55d5a5cea5f
mealet <jasonragonter@gmail.com>
2026-05-17 14:32:56 +0300

    test: added test for null literal

--- compiler/zeen-parser/src/expressions.rs
+++ compiler/zeen-parser/src/expressions.rs
@@ -673,4 +673,31 @@ mod tests {
             );
         }
     }
+
+    #[test]
+    fn literal_null() {
+        const SRC: &str = "null";
+
+        let src = std::sync::Arc::new(SRC.to_string());
+
+        let mut rodeo = lasso::Rodeo::default();
+        let bump = bumpalo::Bump::new();
+
+        let mut tokens = zeen_lexer::tokenize(SRC);
+        let mut parser = Parser::new("tests.zn", src, &mut tokens, &bump, &mut rodeo);
+
+        let mut expr_parser = ExprParser::new(&mut parser);
+
+        {
+            let expr = expr_parser.parse_primary().unwrap();
+
+            assert_eq!(
+                expr,
+                &Expression {
+                    kind: ExpressionKind::Literal(expressions::Literal::Null),
+                    span: (0, "null".len()).into()
+                }
+            );
+        }
+    }
 }
