15251131ee0f4ace96c09c5d0aa1cf30fa3e5806
mealet <jasonragonter@gmail.com>
2026-06-13 21:17:48 +0300

    feat: process implementing include resolver

--- compiler/zeen-resolve/src/resolvers/include_resolver.rs
+++ compiler/zeen-resolve/src/resolvers/include_resolver.rs
@@ -2,15 +2,100 @@ use std::{
     collections::{HashMap, HashSet},
     fs,
     path::{Path, PathBuf},
-    sync::Arc,
+    sync::{Arc, Mutex},
 };
 
 use bumpalo::Bump;
 use lasso::{Rodeo, Spur};
 use miette::{NamedSource, SourceSpan};
+use smol_str::SmolStr;
 
 use crate::error::ResolveError;
 use zeen_ast::declarations::{Declaration, DeclarationKind};
+
+struct RawModule<'arena> {
+    cannonical_path: PathBuf,
+    decls: &'arena [&'arena Declaration<'arena>],
+    named_src: NamedSource<Arc<String>>,
+}
+
+pub struct ImportResolver<'ctx> {
+    arena: &'ctx Bump,
+    interner: Arc<Mutex<Rodeo>>,
+    context: zeen_driver::CompilationContext,
+
+    modules: HashMap<PathBuf, RawModule<'ctx>>,
+
+    src: Arc<String>,
+    filename: &'ctx str,
+    errors: Vec<ResolveError>,
+}
+
+impl<'ctx> ImportResolver<'ctx> {
+    pub fn new(
+        filename: &'ctx str,
+        src: Arc<String>,
+
+        arena: &'ctx Bump,
+        interner: Arc<Mutex<Rodeo>>,
+        context: zeen_driver::CompilationContext,
+    ) -> Self {
+        Self {
+            arena,
+            interner,
+            context,
+
+            src,
+            filename,
+
+            modules: HashMap::new(),
+            errors: Vec::new(),
+        }
+    }
+
+    fn interner_intern(&mut self, value: impl AsRef<str>) -> lasso::Spur {
+        // compiler is not async/threaded (at least for now), so we're unwrapping lock
+        let mut interner = self.interner.lock().unwrap();
+
+        interner.get_or_intern(value)
+    }
+
+    fn interner_resolve(&self, key: &Spur) -> SmolStr {
+        let interner = self.interner.lock().unwrap();
+        let resolved = interner.resolve(key);
+
+        resolved.into()
+    }
+
+    fn named_src(&self) -> NamedSource<Arc<String>> {
+        let src_ref = Arc::clone(&self.src);
+
+        miette::NamedSource::new(self.filename, src_ref)
+    }
+
+    pub fn resolve(
+        &mut self,
+        root_path: PathBuf,
+        root_decls: &'ctx [&'ctx Declaration<'ctx>],
+        root_named_src: NamedSource<Arc<String>>,
+    ) -> Result<&'ctx [&'ctx Declaration<'ctx>], Vec<ResolveError>> {
+        let root_cannonical = canonicalize_best_effort(&root_path);
+
+        self.modules.insert(
+            root_cannonical.clone(),
+            RawModule {
+                decls: root_decls,
+                cannonical_path: root_cannonical.clone(),
+                named_src: root_named_src,
+            },
+        );
+
+        let mut visiting: HashSet<PathBuf> = HashSet::new();
+        visiting.insert(root_cannonical.clone());
+
+        todo!()
+    }
+}
 
 fn resolve_use_path(
     raw: &str,
