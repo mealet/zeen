32fe4183e2db136250f025b691235dd2c6c43a50
mealet <jasonragonter@gmail.com>
2026-06-13 18:47:40 +0300

    feat: defined types in `zeen-resolver`

---
+++ compiler/zeen-resolve/src/types.rs
@@ -0,0 +1,95 @@
+use lasso::Spur;
+use miette::SourceSpan;
+use std::collections::HashMap;
+
+use zeen_ast::{Declaration, Expression, Statement, TypeExpr};
+
+/// A simple representation of allocated AST node as a key.
+/// SAFETY: This thing is very dangerous, can be used when you sure your object is live as long as
+/// NodeKey does. In compiler it is used with arena (lives whole program cycle) allocated objects.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct NodeKey(usize);
+
+impl NodeKey {
+    pub fn from_expr(value: &Expression) -> Self {
+        NodeKey(value as *const _ as usize)
+    }
+
+    pub fn from_stmt(value: &Statement) -> Self {
+        NodeKey(value as *const _ as usize)
+    }
+
+    pub fn from_decl(value: &Declaration) -> Self {
+        NodeKey(value as *const _ as usize)
+    }
+}
+
+/// Unique identifier for a resolver definition.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct DefId(u32);
+
+/// Unique identifier for a resolved imported module.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct ModuleId(u32);
+
+/// Resolution instance that kept in symtable.
+#[derive(Debug, Clone, Copy)]
+pub enum Resolution {
+    Def(DefId),          // function, struct, variable, etc.
+    GenericParam(DefId), // scope generic param (functions/structures)
+    SelfValue(DefId),    // `self` inside structures methods
+    SelfType(DefId),     // `Self` alias
+    Builtin,             // i32, u32, ...
+    Module(ModuleId),    // `import .. : alias`
+}
+
+/// Keeping definition info here
+#[derive(Debug, Clone)]
+pub struct DefInfo {
+    pub name: Spur,
+    pub kind: DefKind,
+    pub span: SourceSpan,
+    pub decl: Option<NodeKey>, // may be useful
+}
+
+#[derive(Debug, Clone)]
+pub enum DefKind {
+    Function,
+    Struct,
+    Interface,
+    Enum,
+    EnumVariant,
+    Variable { is_const: bool },
+    Param,
+    GenericParam,
+    ExternVar,
+}
+
+/// Final output of Name Resolver
+#[derive(Debug, Clone)]
+pub struct ResolutionResult {
+    /// Expr -> Resolution
+    pub expr_bindings: HashMap<NodeKey, Resolution>,
+    /// Type Expr -> Resolution
+    pub type_bindings: HashMap<NodeKey, Resolution>,
+
+    /// All known defs
+    pub defs: HashMap<DefId, DefInfo>,
+    /// (struct, interface) -> methods
+    pub impls: HashMap<(DefId, DefId), Vec<DefId>>,
+
+    /// Top-level modules from imports
+    pub modules: HashMap<Spur, ModuleId>,
+}
+
+impl ResolutionResult {
+    pub fn resolution_of_expr(&self, expr: &Expression) -> Option<Resolution> {
+        self.expr_bindings.get(&NodeKey::from_expr(expr)).copied()
+    }
+
+    pub fn resolution_of_type(&self, texpr: &TypeExpr) -> Option<Resolution> {
+        self.type_bindings
+            .get(&NodeKey(texpr as *const _ as usize))
+            .copied()
+    }
+}
