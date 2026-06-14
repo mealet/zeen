768bfba47f8fa3a521d93e289b6dc07b86ef65d8
mealet <jasonragonter@gmail.com>
2026-06-14 21:01:22 +0300

    feat: created main `resolve` fn

--- compiler/zeen-resolve/src/lib.rs
+++ compiler/zeen-resolve/src/lib.rs
@@ -1,6 +1,51 @@
 #![allow(unused)]
 
+use bumpalo::Bump;
+use lasso::Rodeo;
+
+use std::{
+    path::Path,
+    sync::{Arc, Mutex},
+};
+
+use error::ResolveError;
+use resolvers::{include_resolver, name_resolver};
+
+use zeen_ast::Declaration;
+use zeen_driver::CompilationContext;
+
 mod error;
 mod resolution;
 mod resolvers;
 mod symbol_table;
+
+pub fn resolve(
+    filename: Arc<String>,
+    src: Arc<String>,
+
+    entry_path: &Path,
+    entry_program: &[&Declaration<'_>],
+
+    arena: &Bump,
+    interner: Arc<Mutex<Rodeo>>,
+    context: &mut CompilationContext,
+) -> Result<(), Vec<ResolveError>> {
+    let mut include_resolver = include_resolver::IncludeResolver::new(
+        Arc::clone(&filename),
+        Arc::clone(&src),
+        arena,
+        Arc::clone(&interner),
+        context,
+    );
+
+    let resolved_program = include_resolver.resolve(
+        entry_path.to_path_buf(),
+        entry_program,
+        miette::NamedSource::new(filename.as_str(), Arc::clone(&src)),
+    )?;
+
+    let mut name_resolver = name_resolver::NameResolver::new(filename, src, arena, interner);
+    name_resolver.resolve_module(resolved_program)?;
+
+    Ok(())
+}
--- compiler/zeen-resolve/src/resolvers/name_resolver.rs
+++ compiler/zeen-resolve/src/resolvers/name_resolver.rs
@@ -80,8 +80,27 @@ impl<'ctx> NameResolver<'ctx> {
 
     // -> resolve functions
 
-    pub fn resolve_module(&mut self, decls: &'ctx [&'ctx Declaration<'ctx>]) {
-        todo!()
+    pub fn resolve_module(
+        mut self,
+        decls: &'ctx [&'ctx Declaration<'ctx>],
+    ) -> Result<(), Vec<ResolveError>> {
+        for decl in decls {
+            self.declare_toplevel(decl);
+        }
+
+        if !self.errors.is_empty() {
+            return Err(self.errors);
+        }
+
+        for decl in decls {
+            // in future
+        }
+
+        if !self.errors.is_empty() {
+            return Err(self.errors);
+        }
+
+        Ok(())
     }
 
     fn declare_toplevel(&mut self, decl: &'ctx Declaration<'ctx>) {
