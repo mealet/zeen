0dd70543cffbd9d91224b8bd7d619257131969e8
mealet <jasonragonter@gmail.com>
2026-06-18 15:14:30 +0300

    feat: implemented all HirStmtKind's

--- compiler/zeen-hir/src/stmt.rs
+++ compiler/zeen-hir/src/stmt.rs
@@ -1,14 +1,62 @@
 use zeen_ast::Source;
 use zeen_resolve::DefId;
 
-use crate::HirId;
+use lasso::Spur;
+use miette::SourceSpan;
+use std::rc::Rc;
 
-#[derive(Debug)]
+use crate::{HirId, expr::HirExpr, types::HirTypeExpr};
+
+#[derive(Debug, Clone)]
 pub struct HirStmt {
     pub id: HirId,
     pub kind: HirStmtKind,
     pub source: Source,
 }
 
-#[derive(Debug)]
-pub enum HirStmtKind {}
+#[derive(Debug, Clone)]
+pub enum HirStmtKind {
+    Let {
+        def_id: DefId,
+        name: Spur,
+        explicit_type: Option<Rc<HirTypeExpr>>,
+        value: Option<Rc<HirExpr>>,
+        is_const: bool,
+    },
+
+    Assign {
+        object: Rc<HirExpr>,
+        value: Rc<HirExpr>,
+    },
+
+    CompoundAssign {
+        object: Rc<HirExpr>,
+        value: Rc<HirExpr>,
+        op: zeen_ast::expressions::BinaryOp,
+    },
+
+    Return {
+        value: Option<Rc<HirExpr>>,
+    },
+
+    Defer {
+        body: Rc<HirStmt>,
+    },
+
+    Break,
+
+    While {
+        condition: Rc<HirExpr>,
+        block: Rc<HirStmt>,
+    },
+
+    For {
+        def_id: DefId,
+        varname: (Spur, SourceSpan),
+        iterator: Rc<HirExpr>,
+        block: Rc<HirStmt>,
+    },
+
+    Expr(Rc<HirExpr>),
+    Error,
+}
