179a0ffc2a87d60d7415e0f96f32470a45ae7e81
mealet <jasonragonter@gmail.com>
2026-06-18 21:12:11 +0300

    feat: added HirLowering struct (without implementations)

--- compiler/zeen-hir/src/lib.rs
+++ compiler/zeen-hir/src/lib.rs
@@ -5,8 +5,13 @@ use std::rc::Rc;
 use lasso::Spur;
 use miette::SourceSpan;
 
-use zeen_ast::Source;
-use zeen_resolve::DefId;
+use zeen_ast::{
+    declarations::{Declaration, DeclarationKind, FnParam, GenericType},
+    expressions::{Expression, ExpressionKind},
+    statements::{Statement, StatementKind},
+    types::{TypeExpr, TypeKind},
+};
+use zeen_resolve::{DefId, Resolution, ResolutionResult};
 
 pub mod decl;
 pub mod expr;
@@ -23,7 +28,10 @@ pub struct HirModule {
     pub decls: Vec<Rc<decl::HirDecl>>,
 }
 
-// ====| Public Exports |====
+// No, this is not AI generated, comments are made by me,
+// this just looks fine for me.
+
+// =========| Public Exports |=========
 
 pub use decl::{
     HirDecl, HirDeclKind, HirEnum, HirEnumVariant, HirField, HirFn, HirGenericParam, HirImplement,
@@ -33,3 +41,57 @@ pub use decl::{
 pub use expr::{HirExpr, HirExprKind, HirFieldInit};
 pub use stmt::{HirStmt, HirStmtKind};
 pub use types::{HirTypeExpr, HirTypeKind};
+
+// =========| HIR Lowering |=========
+
+pub struct HirLowering<'res> {
+    resolution: &'res ResolutionResult,
+    next_id: u32,
+}
+
+impl<'res> HirLowering<'res> {
+    pub fn new(resolution: &'res ResolutionResult) -> Self {
+        Self {
+            resolution,
+            next_id: 0,
+        }
+    }
+
+    fn fresh_id(&mut self) -> HirId {
+        let id = HirId(self.next_id);
+        self.next_id += 1;
+        id
+    }
+
+    // ==> Entry Point
+
+    pub fn lower_module<'ctx>(&mut self, decls: &'ctx [&'ctx Declaration<'ctx>]) -> HirModule {
+        todo!()
+    }
+
+    // ==> Lowering Functions
+    
+    // > Declarations
+
+    fn lower_decl<'ctx>(&mut self, decl: &'ctx Declaration<'ctx>) -> Option<Rc<HirDecl>> {
+        todo!()
+    }
+
+    // > Statements
+    
+    fn lower_stmt<'ctx>(&mut self, stmt: &'ctx Statement<'ctx>) -> HirStmt {
+        todo!()
+    }
+
+    // > Expressions
+
+    fn lower_expr<'ctx>(&mut self, expr: &'ctx Expression<'ctx>) -> HirExpr {
+        todo!()
+    }
+
+    // > Types
+
+    fn lower_type<'ctx>(&mut self, ty: &'ctx TypeExpr<'ctx>) -> HirTypeExpr {
+        todo!()
+    }
+}
