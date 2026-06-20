bfd558733724a17e68a08aeb7670ad1bd6e2c4b5
mealet <jasonragonter@gmail.com>
2026-06-20 21:56:00 +0300

    feat: created FnCtx stack for keeping current function context

---
+++ compiler/zeen-typecheck/src/context.rs
@@ -0,0 +1,62 @@
+use std::collections::HashMap;
+
+use crate::types::TypeId;
+use zeen_resolve::DefId;
+
+#[derive(Debug)]
+pub struct FnCtx {
+    pub return_type: TypeId,
+    pub self_type: Option<TypeId>,
+    pub generic_bindings: HashMap<DefId, TypeId>,
+    pub loop_depth: u32,
+}
+
+pub struct TypeCheckCtx {
+    stack: Vec<FnCtx>,
+}
+
+impl TypeCheckCtx {
+    pub fn new() -> Self {
+        Self { stack: Vec::new() }
+    }
+
+    pub fn push_fn(&mut self, ctx: FnCtx) {
+        self.stack.push(ctx);
+    }
+
+    pub fn pop_fn(&mut self) {
+        let _ = self.stack.pop();
+    }
+
+    pub fn current(&self) -> &FnCtx {
+        self.stack
+            .last()
+            .expect("Current context called outside of any function body")
+    }
+
+    pub fn current_mut(&mut self) -> &mut FnCtx {
+        self.stack
+            .last_mut()
+            .expect("Current context called outside of any function body")
+    }
+
+    pub fn in_loop(&self) -> bool {
+        self.stack.last().is_some_and(|ctx| ctx.loop_depth > 0)
+    }
+
+    pub fn enter_loop(&mut self) {
+        self.current_mut().loop_depth += 1;
+    }
+
+    pub fn exit_loop(&mut self) {
+        self.current_mut().loop_depth -= 1;
+    }
+
+    pub fn generic_binding(&self, def_id: DefId) -> Option<TypeId> {
+        self.stack.last()?.generic_bindings.get(&def_id).copied()
+    }
+
+    pub fn bind_generic(&mut self, def_id: DefId, ty: TypeId) {
+        self.current_mut().generic_bindings.insert(def_id, ty);
+    }
+}
--- compiler/zeen-typecheck/src/lib.rs
+++ compiler/zeen-typecheck/src/lib.rs
@@ -1,3 +1,4 @@
 #![allow(unused)]
 
 mod types;
+mod context;
