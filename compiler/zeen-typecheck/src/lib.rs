bddce188471249e2690f1261baf0517d23681846
mealet <jasonragonter@gmail.com>
2026-06-28 15:31:58 +0300

    todo: added todo comment

--- compiler/zeen-typecheck/src/lib.rs
+++ compiler/zeen-typecheck/src/lib.rs
@@ -657,6 +657,9 @@ impl<'res> TypeChecker<'res> {
         args: &[Rc<HirExpr>],
         source: Source,
     ) -> TypeId {
+        // TODO: Finish some macro calls, add special verificators where necessary and implement
+        // formats unwrappers for @print, @println and @format
+
         match kind.0 {
             HirMacroKind::Print | HirMacroKind::Println => {
                 for arg in args {
