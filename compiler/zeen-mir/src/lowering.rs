04c063eb90db646a0f73c298af8ec1ff8d6f4f87
mealet <jasonragonter@gmail.com>
2026-07-27 18:50:26 +0300

    style: fixed formatting in place

--- compiler/zeen-mir/src/lowering.rs
+++ compiler/zeen-mir/src/lowering.rs
@@ -1125,11 +1125,13 @@ impl<'ctx> MirLowering<'ctx> {
             Type::Array { .. } => iter_place.clone().index(counter),
             Type::Slice { .. } => {
                 let mut ptr_place = iter_place.clone();
-        ptr_place.projection.push(PlaceElem::SlicePtr);
+
+                ptr_place.projection.push(PlaceElem::SlicePtr);
                 ptr_place.index(counter)
             }
             _ => unreachable!(),
         };
+
         let elem_operand = self.place_to_operand(elem_place, elem_ty);
 
         fb.push_stmt(
