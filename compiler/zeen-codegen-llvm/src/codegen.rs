31db294d0e1ab50093e24880e845e6657b6711c2
mealet <jasonragonter@gmail.com>
2026-08-28 20:24:08 +0300

    style: clippy and fmt applied

--- compiler/zeen-codegen-llvm/src/codegen.rs
+++ compiler/zeen-codegen-llvm/src/codegen.rs
@@ -18,7 +18,9 @@ use inkwell::{
 };
 use lasso::{Rodeo, Spur};
 use zeen_ast::{
-    Source, expressions::{BinaryOp, UnaryOp}, types::BuiltinType
+    Source,
+    expressions::{BinaryOp, UnaryOp},
+    types::BuiltinType,
 };
 use zeen_driver::CompilationMode;
 use zeen_hir::HirMacroKind;
@@ -1666,7 +1668,7 @@ impl<'ctx, 'prog> CodeGen<'ctx, 'prog> {
                     *target,
                     func,
                     fn_id,
-                    source
+                    source,
                 );
             }
 
@@ -1972,13 +1974,18 @@ impl<'ctx, 'prog> CodeGen<'ctx, 'prog> {
 
                 let source = source.clone().expect("unhandled None source");
 
-                let debug_source = &source.src.inner()[source.span.offset()..source.span.offset() + source.span.len()];
+                let debug_source = &source.src.inner()
+                    [source.span.offset()..source.span.offset() + source.span.len()];
                 let debug_inner = debug_source
                     .strip_prefix("@dbg(")
                     .and_then(|s| s.strip_suffix(')'))
                     .unwrap_or(debug_source);
 
-                let debug_location = format!("{}:{}", source.src.name(), source_line(source.src.inner(), source.span.offset()));
+                let debug_location = format!(
+                    "{}:{}",
+                    source.src.name(),
+                    source_line(source.src.inner(), source.span.offset())
+                );
 
                 let printf = self.get_or_declare_runtime_fn(
                     "printf",
