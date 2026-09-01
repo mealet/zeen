ba871343dce41ca383411b62f62dc8cb3b814e4f
mealet <jasonragonter@gmail.com>
2026-09-01 10:53:30 +0300

    chore: embed std files in build.rs

--- compiler/zeen/build.rs
+++ compiler/zeen/build.rs
@@ -61,4 +61,62 @@ pub static CORE_FILES: &[CoreFile] = &[
         fs::write(out_dir.join("core_files.rs"), output).unwrap();
     }
     // < Core Modules Builtin Import
+
+    // @ Std Modules Builtin Import
+    {
+        // project/lib/std
+        let std_dir = Path::new("../../lib/std");
+
+        println!("cargo:rerun-if-changed={}", std_dir.display());
+
+        let mut output = String::new();
+
+        output.push_str(
+            r#"
+pub struct StdFile {
+    pub name: &'static str,
+    pub value: &'static str,
+}
+
+impl StdFile {
+    pub fn to_basic(&self) -> (&'static str, &'static str) {
+        (self.name, self.value)
+    }
+}
+
+pub static STD_FILES: &[StdFile] = &[
+            "#,
+        );
+
+        for entry in fs::read_dir(std_dir).unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path();
+
+            if !path.is_file() {
+                continue;
+            }
+
+            if path.extension().and_then(|ext| ext.to_str()) != Some("zn") {
+                continue;
+            }
+
+            let stem = path.file_stem().unwrap().to_str().unwrap();
+            let content = fs::read_to_string(&path).unwrap();
+
+            output.push_str(&format!(
+                r#"  StdFile {{
+    name: "std.{stem}",
+    value: {content:?},
+  }},"#
+            ));
+
+            output.push('\n');
+        }
+
+        output.push_str("];\n");
+
+        let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+        fs::write(out_dir.join("std_files.rs"), output).unwrap();
+    }
+    // < Std Modules Builtin Import
 }
