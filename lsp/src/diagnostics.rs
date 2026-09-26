2407d0cca9b59db35b63aeea003fad9061bce2d3
mealet <jasonragonter@gmail.com>
2026-09-26 19:13:06 +0300

    chore: added lasso and bumpalo dependencies

--- lsp/src/diagnostics.rs
+++ lsp/src/diagnostics.rs
@@ -1,1 +1,13 @@
+use crate::position;
+use std::{cell::RefCell, rc::Rc, sync::Arc};
+
 use miette::Diagnostic as _;
+use tower_lsp_server::ls_types::{Diagnostic, DiagnosticSeverity};
+
+pub fn check(text: impl AsRef<str>) -> Vec<Diagnostic> {
+    let filename = Rc::new("in-memory.zn".to_string());
+    let content = Arc::new(text.as_ref().to_string());
+    let interner = Rc::new(RefCell::new(todo!()));
+
+    todo!()
+}
