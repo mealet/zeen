f6341d0bf7e86bc5eaa3dfaa05b6d128fdea02c8
mealet <jasonragonter@gmail.com>
2026-09-26 19:35:41 +0300

    feat: implemented main LSP serve

--- lsp/src/main.rs
+++ lsp/src/main.rs
@@ -4,6 +4,14 @@ mod backend;
 mod diagnostics;
 mod position;
 
-fn main() {
-    println!("Hello, world!");
+use backend::Backend;
+use tower_lsp_server::{LspService, Server};
+
+#[tokio::main]
+async fn main() {
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let (service, socket) = LspService::new(Backend::new);
+    Server::new(stdin, stdout, socket).serve(service).await;
 }
